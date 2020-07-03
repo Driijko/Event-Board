2a14c61d4a71ad1309f55b6773ebf0c93df8f562
Driijko <driijko@gmail.com>
2020-07-03 13:36:11 -0400

    First pass template-version ProjectsList component

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import ProjectSummary from "./ProjectSummary";
+
+export default function ProjectList(props) {
+  return (
+    <div className="project-list section">
+
+      <ProjectSummary />
+
+      <ProjectSummary />
+
+      <ProjectSummary />
+      
+    </div>
+  )
+}
