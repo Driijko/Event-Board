48dd5d156e64d9047dc5a3ec91664a5fa568e518
Driijko <driijko@gmail.com>
2020-07-03 13:08:13 -0400

    Proper integration of Materialize via CDN

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 
+import Navbar from "./components/layouts/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <h1>MarioPlan</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
   );
 }
 
