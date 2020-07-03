1a443f27c08b50c6f2ba0a5facbfdbcfbf1d5631
Driijko <driijko@gmail.com>
2020-07-03 17:51:57 -0400

    First pass at SignIn and SignUp components

---
+++ src/components/auth/SignIn.js
@@ -0,0 +1,43 @@
+import React, {useState} from "react";
+
+export default function SignIn(props) {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+  }
+
+  function handleChange(e) {
+    if (e.target.id === "email") {
+      setEmail(e.target.value);
+    }
+    else if (e.target.id === "password") {
+      setPassword(e.target.value);
+    }
+  }
+
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit} className="white">
+        <h5 className="grey-text text-darken-3">Sign In</h5>
+        <div className="input-field">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" onChange={handleChange} />
+        </div>
+        <div className="input-field">
+          <button className="btn pink lighten-1 z-depth-0">Login</button>
+        </div>
+      </form>
+    </div>
+  )
+}
